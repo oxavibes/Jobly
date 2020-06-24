8e9284163a535cb7186a6f17f33eeda2b298cafc
Stefano <katteos@hotmail.com>
2020-06-24 02:02:21 -0400

    Added Breadcrumb component

---
+++ src/components/Breadcrum.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="alice-bg padding-top-70 padding-bottom-70">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="breadcrumb-area">
+            <h1>Job Listing</h1>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                  <a href="#">Home</a>
+                </li>
+                <li class="breadcrumb-item active" aria-current="page">
+                  Job Listing
+                </li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="breadcrumb-form">
+            <form action="#">
+              <input type="text" placeholder="Enter Keywords" />
+              <button>
+                <i data-feather="search"></i>
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
