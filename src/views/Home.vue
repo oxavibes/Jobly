4a3011bd4c9d4bd66c7593fd9358993f7bd32ba2
Stefano <katteos@hotmail.com>
2020-06-24 00:14:23 -0400

    Updated Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,66 +1,97 @@
 <template>
   <div>
-    <!-- Login Modal -->
-    <app-login-modal></app-login-modal>
-    <!-- Login Modal end -->
-
-    <!-- Register Modal -->
-    <app-register-modal></app-register-modal>
-    <!-- Register Modal end -->
-
     <!-- Banner -->
-    <app-banner></app-banner>
+    <app-banner>
+      <h1>Empleos tecnológicos de calidad.</h1>
+      <p>
+        Jobly te permite postular a trabajos seleccionados en startups y en
+        empresas tecnológicas globales.
+      </p>
+      <div class="short-infos">
+        <div class="info">
+          <h4>5,862</h4>
+          <span>Empleos publicados</span>
+        </div>
+        <div class="info">
+          <h4>240</h4>
+          <span>Compañias</span>
+        </div>
+      </div>
+    </app-banner>
     <!-- Banner end -->
 
     <!-- Search -->
     <app-search></app-search>
     <!-- Search end -->
 
-    <!-- Category -->
-    <app-category></app-category>
-    <!-- Category end -->
-
     <!-- Jobs -->
-    <app-jobs></app-jobs>
+    <app-recent-jobs></app-recent-jobs>
     <!-- Jobs end -->
 
     <!-- Top Companies -->
     <app-top-companies></app-top-companies>
     <!-- Top Companies end -->
 
-    <!-- Testimonial -->
-    <app-testimonial></app-testimonial>
-    <!-- Testimonial end-->
+    <!-- Registration Box -->
+    <div class="section-padding">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="call-to-action-box candidate-box">
+              <div class="icon">
+                <img src="../../public/assets/images/register-box/1.png" alt />
+              </div>
+              <span>¿Eres un</span>
+              <h3>Candidato?</h3>
+              <router-link to="/sign-up">
+                Regístrate aquí
+                <i class="fas fa-arrow-right"></i>
+              </router-link>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="call-to-action-box employer-register">
+              <div class="icon">
+                <img src="../../public/assets/images/register-box/2.png" alt />
+              </div>
+              <span>¿Eres Un</span>
+              <h3>Empleador?</h3>
+              <router-link to="/sign-up">
+                Regístrate aquí
+                <i class="fas fa-arrow-right"></i>
+              </router-link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Registration Box End -->
 
-    <!-- Career advice -->
-    <app-career-advice></app-career-advice>
-    <!-- Career advice end -->
+    <app-newsletter></app-newsletter>
   </div>
 </template>
 
 <script>
-import appLoginModal from "../components/LoginModal";
-import appRegisterModal from "../components/RegisterModal";
 import appBanner from "../components/Banner";
 import appSearch from "../components/Search";
-import appCategory from "../components/Category";
-import appJobs from "../components/Jobs";
+// import appCategory from "../components/Category";
+import appRecentJobs from "../components/RecentJobs";
 import appTopCompanies from "../components/TopCompanies";
-import appTestimonial from "../components/Testimonial";
-import appCareerAdvice from "../components/CareerAdvice";
+import appNewsletter from "../components/Newsletter";
+// import appTestimonial from "../components/Testimonial";
+// import appCareerAdvice from "../components/CareerAdvice";
 
 export default {
   name: "appHome",
   components: {
-    appLoginModal,
-    appRegisterModal,
     appBanner,
     appSearch,
-    appCategory,
-    appJobs,
+    // appCategory,
+    appRecentJobs,
     appTopCompanies,
-    appTestimonial,
-    appCareerAdvice
+    appNewsletter
+    // appTestimonial,
+    // appCareerAdvice
   }
 };
 </script>
