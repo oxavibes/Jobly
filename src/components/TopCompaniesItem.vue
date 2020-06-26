34297cfd4e824a20de8ccf4957e503d474fdc0bd
Stefano <katteos@hotmail.com>
2020-06-26 00:10:04 -0400

    Created TopCompaniesItem component

---
+++ src/components/TopCompaniesItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="company-wrap">
+    <div class="thumb">
+      <a href="#">
+        <img
+          :src="require(`../../public/assets/images/company/${company.logo}`)"
+          class="img-fluid"
+          alt="Company logo"
+        />
+      </a>
+    </div>
+    <div class="body">
+      <h4>
+        <a href="employer-details.html">{{ company.name }}</a>
+      </h4>
+      <span>{{ company.location }}</span>
+      <a href="company-listing.html" class="button">
+        {{ company.vacancies }}
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TopCompaniesItem",
+  props: {
+    company: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+</style>
