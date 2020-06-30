b86211879d6f3378aa430494de13b16cba9c8b97
Stefano <katteos@hotmail.com>
2020-06-30 00:16:23 -0400

    Removed unnecessay packages

--- src/globalComponents.js
+++ src/globalComponents.js
@@ -1,12 +1,8 @@
-/* Owl Carousel */
-import OwlCarousel from "vue-owl-carousel";
-import VueCarousel from "vue-carousel";
 
+/*
 const globalComponents = {
   install(Vue) {
-    Vue.component("owlCarousel", OwlCarousel);
-    Vue.component("vueCarousel", VueCarousel)
   }
-}
+}*/
 
-export default globalComponents;
+// export default globalComponents;
--- src/main.js
+++ src/main.js
@@ -1,23 +1,19 @@
-// import $ from "jquery";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import bootstrap from "bootstrap-vue"
 import globalComponents from "./globalComponents";
 
-Vue.use(bootstrap)
-Vue.use(globalComponents)
+Vue.use(globalComponents);
 
 Vue.config.productionTip = false;
 
 Vue.mixin({
   created() {
-    console.log('[created] ' + this.$options.name)
-  },
+    console.log("[created] " + this.$options.name);
+  }
 });
 
-// window.$ = $;
 
 new Vue({
   router,
