cc7ddf2bf9a4f0d9a5814734b8d92166ef6dde07
Stefano <katteos@hotmail.com>
2020-07-22 06:19:22 -0400

    Add CallToAction component

---
+++ src/components/CallToAction.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="call-to-action-bg padding-top-90 padding-bottom-90">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="call-to-action-2">
+            <div class="call-to-action-content">
+              <h2>For Find Your Dream Job or Candidate</h2>
+              <p>
+                Add resume or post a job. Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit. Ut elit tellus, luctus nec.
+              </p>
+            </div>
+            <div class="call-to-action-button">
+              <a href="add-resume.html" class="button">Add Resume</a>
+              <span>Or</span>
+              <a href="post-job.html" class="button">Post A Job</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CallToAction"
+};
+</script>
+
+<style></style>
