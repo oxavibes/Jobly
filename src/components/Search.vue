87767611a73baa14be45d623683e8238193a9d99
Stefano <katteos@hotmail.com>
2020-07-22 06:22:12 -0400

    Add SelectPicker component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -7,31 +7,17 @@
             <div class="searchAndFilter">
               <form action="#" class="search-form">
                 <input type="text" placeholder="Palabras claves" />
-                <!-- <v-select
-                  v-model="selectedLocation"
-                  :options="locationOptions"
-                  class="select"
-                  id="search-location"
-                ></v-select> -->
-                <select class="selectpicker" id="search-location">
-                  <option value selected>Ubicación</option>
-                  <option value="california">California</option>
-                  <option value="las-vegas">Las Vegas</option>
-                  <option value="new-work">New Work</option>
-                  <option value="carolina">Carolina</option>
-                  <option value="chicago">Chicago</option>
-                  <option value="silicon-vally">Silicon Vally</option>
-                  <option value="washington">Washington DC</option>
-                  <option value="neveda">Neveda</option>
-                </select>
-                <select class="selectpicker" id="search-category">
-                  <option value selected>Categoría</option>
-                  <option value="real-state">Real State</option>
-                  <option value="vehicales">Vehicales</option>
-                  <option value="electronics">Electronics</option>
-                  <option value="beauty">Beauty</option>
-                  <option value="furnitures">Furnitures</option>
-                </select>
+                <select-picker
+                  :selected.sync="selectedLocation"
+                  :options.sync="locationOptions"
+                ></select-picker>
+
+                <select-picker
+                  :selected.sync="selectedCategory"
+                  :options.sync="categoryOptions"
+                >
+                </select-picker>
+
                 <button class="button primary-bg">
                   <i class="fas fa-search"></i>Buscar empleo
                 </button>
@@ -39,64 +25,13 @@
               <div class="filter-categories">
                 <h4>Categorías</h4>
                 <ul>
-                  <li>
+                  <li
+                    v-for="({ icon, text, quantity }, index) in categoryOptions"
+                    :key="index"
+                  >
                     <a href="job-listing.html">
-                      <bar-chart-2-icon></bar-chart-2-icon> Diseño / UX
-                      <span>(233)</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="job-listing.html">
-                      <edit-3-icon></edit-3-icon> Programación
-                      <span>(46)</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="job-listing.html">
-                      <feather-icon></feather-icon> Data Science & Analytics
-                      <span>(156)</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="job-listing.html">
-                      <briefcase-icon></briefcase-icon> Desarrollo Mobile
-                      <span>(98)</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="job-listing.html">
-                      <package-icon></package-icon> Engineer & Architects
-                      <span>(188)</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="job-listing.html">
-                      <pie-chart-icon></pie-chart-icon> Marketing Dgital
-                      <span>(124)</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="job-listing.html">
-                      <command-icon></command-icon> SysAdmin / Devops / Qa
-                      <span>(584)</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="job-listing.html">
-                      <globe-icon></globe-icon> Soporte
-                      <span>(233)</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="job-listing.html">
-                      <headphones-icon></headphones-icon> Innovacion y Agilidad
-                      <span>(15)</span>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="job-listing.html">
-                      <radio-icon></radio-icon> Telecomunicaciónes
-                      <span>(03)</span>
+                      <component :is="icon" /> {{ text }}
+                      <span>{{ quantity }}</span>
                     </a>
                   </li>
                 </ul>
@@ -110,7 +45,8 @@
 </template>
 
 <script>
-// import VSelect from "@alfsnd/vue-bootstrap-select";
+/* eslint-disable vue/no-unused-components */
+import SelectPicker from "../components/SelectPicker";
 import {
   BarChart2Icon,
   Edit3Icon,
@@ -127,6 +63,7 @@ import {
 export default {
   name: "search",
   components: {
+    SelectPicker,
     BarChart2Icon,
     Edit3Icon,
     FeatherIcon,
@@ -141,6 +78,7 @@ export default {
   data() {
     return {
       selectedLocation: "ubicacion",
+      selectedCategory: "category",
       locationOptions: [
         { value: "ubicacion", text: "Ubicación" },
         { value: "california", text: "California" },
@@ -151,6 +89,74 @@ export default {
         { value: "silicon-valley", text: "Silicon Valley" },
         { value: "washington", text: "Washington DC" },
         { value: "neveda", text: "Neveda" }
+      ],
+      categoryOptions: [
+        {
+          value: "category",
+          text: "Categoría",
+          quantity: 233,
+          icon: "BarChart2Icon"
+        },
+        {
+          value: "design",
+          text: "Diseño / UX",
+          quantity: 46,
+          icon: "Edit3Icon"
+        },
+        {
+          value: "programming",
+          text: "Programación",
+          quantity: 156,
+          icon: "FeatherIcon"
+        },
+        {
+          value: "data-science",
+          text: "Data Science",
+          quantity: 98,
+          icon: "BriefcaseIcon"
+        },
+        {
+          value: "mobile-dev",
+          text: "Desarrollo Mobile",
+          quantity: 188,
+          icon: "PackageIcon"
+        },
+        {
+          value: "engineering",
+          text: "Engineer & Architects",
+          quantity: 124,
+          icon: "PieChartIcon"
+        },
+        {
+          value: "marketing",
+          text: "Marketing Digital",
+          quantity: 584,
+          icon: "CommandIcon"
+        },
+        {
+          value: "sysadmin",
+          text: "SysAdmin / Devops / Qa ",
+          quantity: 233,
+          icon: "GlobeIcon"
+        },
+        {
+          value: "soporte",
+          text: "Soporte",
+          quantity: 15,
+          icon: "HeadphonesIcon"
+        },
+        {
+          value: "innovation",
+          text: "Innovacion y Agilidad ",
+          quantity: 5,
+          icon: "RadioIcon"
+        },
+        {
+          value: "telecommunications",
+          text: "Telecomunicaciónes",
+          quantity: 15,
+          icon: "BriefcaseIcon"
+        }
       ]
     };
   }
---
+++ src/components/SelectPicker.vue
@@ -0,0 +1,43 @@
+<template>
+  <select class="selectpicker" v-model="selectModel">
+    <option
+      :value="value"
+      v-for="({ value, text }, index) in options"
+      :key="index"
+    >
+      {{ text }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "SelectPicker",
+  props: {
+    options: {
+      type: Array
+    },
+    selected: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      selectModel: this.selected
+    };
+  },
+  mounted() {
+    $(this.$el).selectpicker();
+  },
+  watch: {
+    selected(value) {
+      this.selectModel = value;
+    },
+    selectModel(value) {
+      this.$emit("update:selected", value);
+    }
+  }
+};
+</script>
+
+<style></style>
