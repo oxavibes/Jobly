d2d3abc4caf8996bfaf9051560bcdfed37078076
Stefano <katteos@hotmail.com>
2020-08-03 23:53:29 -0400

    Add CandidateProfile View

---
+++ src/views/Candidate/CandidateProfile.vue
@@ -0,0 +1,127 @@
+<template>
+  <form action="#" class="dashboard-form">
+    <div class="dashboard-section upload-profile-photo">
+      <div class="update-photo">
+        <img
+          class="image"
+          src="../../../public/assets/dashboard/images/user-1.jpg"
+          alt=""
+        />
+      </div>
+      <div class="file-upload">
+        <input type="file" class="file-input" />Change Avatar
+      </div>
+    </div>
+    <div class="dashboard-section basic-info-input">
+      <h4><i data-feather="user-check"></i>Basic Info</h4>
+      <div class="form-group row">
+        <label class="col-sm-3 col-form-label">Full Name</label>
+        <div class="col-sm-9">
+          <input type="text" class="form-control" placeholder="Full Name" />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-3 col-form-label">Username</label>
+        <div class="col-sm-9">
+          <input type="text" class="form-control" placeholder="@username" />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-3 col-form-label">Email Address</label>
+        <div class="col-sm-9">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="email@example.com"
+          />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-3 col-form-label">Phone</label>
+        <div class="col-sm-9">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="+55 123 4563 4643"
+          />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-3 col-form-label">Address</label>
+        <div class="col-sm-9">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Washington D.C"
+          />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-3 col-form-label">Indestry Expertise</label>
+        <div class="col-sm-9">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="UI & UX Designer"
+          />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-3 col-form-label">About Me</label>
+        <div class="col-sm-9">
+          <textarea
+            class="form-control"
+            placeholder="Introduce Yourself"
+          ></textarea>
+        </div>
+      </div>
+    </div>
+    <div class="dashboard-section basic-info-input">
+      <h4><i data-feather="lock"></i>Change Password</h4>
+      <div class="form-group row">
+        <label class="col-sm-3 col-form-label">Current Password</label>
+        <div class="col-sm-9">
+          <input
+            type="password"
+            class="form-control"
+            placeholder="Current Password"
+          />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-3 col-form-label">New Password</label>
+        <div class="col-sm-9">
+          <input
+            type="password"
+            class="form-control"
+            placeholder="New Password"
+          />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-3 col-form-label">Retype Password</label>
+        <div class="col-sm-9">
+          <input
+            type="password"
+            class="form-control"
+            placeholder="Retype Password"
+          />
+        </div>
+      </div>
+      <div class="form-group row">
+        <label class="col-sm-3 col-form-label"></label>
+        <div class="col-sm-9">
+          <button class="button">Save Change</button>
+        </div>
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "CandidateProfile"
+};
+</script>
+
+<style></style>
