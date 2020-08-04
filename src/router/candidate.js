543e8a0ce90e89ac6290d9ac4dfe1f0c134d3e01
Stefano <katteos@hotmail.com>
2020-08-03 23:55:17 -0400

    Update Candidate Routes

--- src/router/candidate.js
+++ src/router/candidate.js
@@ -2,6 +2,7 @@
 import DefaultLayout from "../layout/DefaultLayout";
 
 //Views
+import CandidateHome from "../views/Candidate/CandidateHome";
 import CandidateProfile from "../views/Candidate/CandidateProfile";
 import CandidateDashboard from "../views/Candidate/CandidateDashboard";
 import CandidateAppliedJobs from "../views/Candidate/CandidateAppliedJobs";
@@ -9,46 +10,41 @@ import CandidateBookmarkedJobs from "../views/Candidate/CandidateBookmarkedJobs"
 
 export const routes = [
   {
-    path: "/candidate-dashboard",
-    name: "candidate-dashboard",
-    component: CandidateDashboard,
-    meta: {
-      layout: DefaultLayout,
-      breadcrumb: [
-        {
-          name: "Inicio",
-          link: "/"
-        },
-        {
-          name: "Resumen",
-          link: "/candidate/dashboard",
-          active: true
+    path: "/candidate",
+    component: CandidateHome,
+    children: [
+      {
+        path: "",
+        name: "candidate-dashboard",
+        component: CandidateDashboard,
+        meta: {
+          layout: DefaultLayout
         }
-      ]
-    }
-  },
-  {
-    path: "/candidate-profile",
-    name: "candidate-profile",
-    component: CandidateProfile,
-    meta: {
-      layout: DefaultLayout
-    }
-  },
-  {
-    path: "/candidate-applied-jobs",
-    name: "candidate-applied-jobs",
-    component: CandidateAppliedJobs,
-    meta: {
-      layout: DefaultLayout
-    }
-  },
-  {
-    path: "/candidate-bookmarked-jobs",
-    name: "candidate-bookmarked-jobs",
-    component: CandidateBookmarkedJobs,
-    meta: {
-      layout: DefaultLayout
-    }
+      },
+      {
+        path: "profile",
+        name: "candidate-profile",
+        component: CandidateProfile,
+        meta: {
+          layout: DefaultLayout
+        }
+      },
+      {
+        path: "applied-jobs",
+        name: "candidate-applied-jobs",
+        component: CandidateAppliedJobs,
+        meta: {
+          layout: DefaultLayout
+        }
+      },
+      {
+        path: "bookmarked-jobs",
+        name: "candidate-bookmarked-jobs",
+        component: CandidateBookmarkedJobs,
+        meta: {
+          layout: DefaultLayout
+        }
+      }
+    ]
   }
 ];
