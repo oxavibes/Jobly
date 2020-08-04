8f6c88643a1bdc9435f909ae4ba88ea4ee5224de
Stefano <katteos@hotmail.com>
2020-08-03 23:52:32 -0400

    Add CandidateHome View

---
+++ src/views/Candidate/CandidateHome.vue
@@ -0,0 +1,46 @@
+<template>
+  <dashboard :menu="menu">
+    <template v-slot:content>
+      <router-view></router-view>
+    </template>
+  </dashboard>
+</template>
+
+<script>
+import Dashboard from "../../components/VDashboard";
+
+export default {
+  name: "CandidateHome",
+  components: {
+    Dashboard
+  },
+  data() {
+    return {
+      menu: [
+        {
+          title: "Tablero",
+          icon: "fas fa-home",
+          route: "candidate-dashboard"
+        },
+        {
+          title: "Perfil",
+          icon: "fas fa-user",
+          route: "candidate-profile"
+        },
+        {
+          title: "Postulaciones",
+          icon: "fas fa-check-square",
+          route: "candidate-applied-jobs"
+        },
+        {
+          title: "Empleos guardados",
+          icon: "fas fa-heart",
+          route: "candidate-bookmarked-jobs"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
